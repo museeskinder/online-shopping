cae1a739dff27665ba93a9bb9dd098150beeebdd
museeskinder <eskindermuse@gmail.com>
2025-01-26 12:48:25 +0300

    connection to db

---
+++ server/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$serverName = "localhost";
+$userName = "root";
+$dbName= "on_db";
+
+$conn = mysqli_connect($serverName, $userName, $dbName)
+    or die('failed to connect to database!');
+
+
+
+?>
