ccf7da2525dbb83da3ca54d7c4239160005c9937
museeskinder <eskindermuse@gmail.com>
2025-01-30 16:00:40 +0300

    implemented user login logic

--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<?php
+    session_start();
+    include('server/connection.php');
+
+    if(isset($_POST['login_btn'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $getUser = $conn->prepare("SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email=? AND user_password = ? LIMIT 1");
+        $getUser->bind_param('ss', $email, md5($password));
+
+        if($getUser->execute()) {
+            $getUser->bind_result($user_id, $user_name, $user_email, $user_password);
+            $getUser->store_result();
+
+            if($getUser->num_rows() == 1) {
+                $getUser->fetch();
+
+                $_SESSION['user_id'] = $user_id;
+                $_SESSION['user_name'] = $user_name;
+                $_SESSION['user_email'] = $user_email;
+                $_SESSION['user_logged'] = true;
+                
+                header('location: login.php?message=logged in successfully');
+
+            }
+            else
+                header('location: login.php?error=could not verify the account');
+        }
+        else {
+            header('location: login.php?error=something went wrong');
+        }
+
+    }
+
+    if(isset($_SESSION['user_logged'])) {
+        header('location: account.php');
+        exit;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,7 +75,8 @@
             <h2 class="font-weight-bold">Login</h2>
         </div>
         <div class="mx-auto container">
-            <form action="#" id="login-form">
+            <form action="login.php" method="POST" id="login-form">
+                <p style="color: red"><?php if(isset($_GET['error'])) echo $_GET['error'] ?></p>
                 <div class="form-group">
                     <label>Email</label>
                     <input type="text" class="form-control" id="login-email" name="email" placeholder="Email" required>
@@ -45,10 +86,10 @@
                     <input type="password" class="form-control" id="login-password" name="password" placeholder="Password" required>
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn" id="login-btn" value="Login">
+                    <input type="submit" class="btn" id="login-btn" value="Login" name="login_btn">
                 </div>
                 <div class="form-group">
-                    <a id="register-url" class="btn">Don't Have account? Register</a>
+                    <a id="register-url" href="register.php" class="btn">Don't Have account? Register</a>
                 </div>
             </form>
         </div>
@@ -98,4 +139,10 @@
     <p class="copy">&#169 2024 All rights reserved.</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        let first = "<?php echo $password ?>";
+        let second = "<?php echo $user_password?>";
+        console.log(first);
+        console.log(second);
+    </script>
 </body>
